384edbe432f82472bab203ab6bf549a8cf711677
AmirHosseinFathi <f.amirhosssein81@gmail.com>
2023-06-08 09:40:21 +0430

    added a second page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
-})
+app.get('/test', (_req, res) => {
+  res.status(200).send('Hello world');
+});
+
+app.get('/test/', (_req, res) => {
+  res.status(200).send('Welcome to the second Page');
+});
 module.exports = app;
