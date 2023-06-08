6fb112bac773b48f6e90abd030ec5e9a39ed7e17
AmirHosseinFathi <f.amirhosssein81@gmail.com>
2023-06-08 10:21:24 +0430

    Added more Gets and Tests

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ app.get('/test', (_req, res) => {
   res.status(200).send('Hello world');
 });
 
-app.get('/test/', (_req, res) => {
+app.get('/test/second', (_req, res) => {
   res.status(200).send('Welcome to the second Page');
+});
+
+app.get('/test/third', (_req, res) => {
+  res.status(200).send('Welcome to the third Page');
+});
+
+app.get('/test/fourth', (_req, res) => {
+  res.status(200).send('Welcome to the fourth Page');
 });
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -1,11 +1,39 @@
-const app = require("./app")
-const supertest = require("supertest")
-const request = supertest(app)
+const { describe } = require('yargs');
+const app = require('./app');
+const supertest = require('supertest');
+const { it } = require('node:test');
+const exp = require('constants');
+const request = supertest(app);
 
-describe("/test endpoint", () => {
-    it("should return a response", async () => {
-        const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
+describe('/test endpoint', () => {
+  it('should return a response', async () => {
+    const response = await request.get('/test');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello world');
+  });
+});
+
+describe('/test/second', () => {
+    it('should return a response',async()=>{
+        const response = await request.get('/test/second');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Welcome to the second Page');
     })
-})
+});
+
+describe('/test/third', () => {
+    it('should return a response',async()=>{
+        const response = await request.get('/test/third');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Welcome to the third Page');
+    })
+});
+
+
+describe('/test/fourth', () => {
+    it('should return a response',async()=>{
+        const response = await request.get('/test/fourth');
+        expect(response.status).toBe(200);
+        expect(response.text).toBe('Welcome to the fourth Page');
+    })
+});
